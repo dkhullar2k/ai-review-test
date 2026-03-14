da21f4c1a3de53492526ab2085c8d72cf26adbac
dkhullar2k <dhruvkhullar2k@gmail.com>
2026-03-14 13:55:27 +0530

    Modify loop to print numbered 'Hello World' messages

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    for i := 0; i < 5; i++ {
-        fmt.Println("Hello World")
+    for i := 1; i <= 5; i++ {
+        fmt.Printf("Hello World #%d\n", i)
     }
 }
